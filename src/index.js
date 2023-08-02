e900b83fb6dae4f3b2584fdf1a3fe33f287516dc
Slava Kamenev <slava-stavropol@yandex.ru>
2023-08-02 19:16:11 +0300

    cooperation done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-function setupCounter(element) {
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind("[data-fancybox]", {
+/*   tpl: {
+    closeButton:
+      '<button data-fancybox-close class="f-button is-close-btn" title="Закрыть">' +
+      '<svg width="15" height="15" viewBox="15" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="15" height="2.7" fill="white" /><rect y="6" x="3" width="12" height="2.7" fill="white" /><rect y="12" x="6" width="9" height="2.7" fill="white" /></svg>' +
+      "</button>",
+  }, */
+});
+
+
+/* function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
@@ -10,3 +23,4 @@ function setupCounter(element) {
 
 
 setupCounter(document.querySelector('#counter'))
+ */
