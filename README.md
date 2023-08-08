2cd3dd113cfd5fa1c70dc5f9cb625a04234714d2
Slava Kamenev <slava-stavropol@yandex.ru>
2023-08-08 19:43:53 +0300

    aniation AOS & guarantees-block

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 1. Отрефакторить заглавную картинку
 2. Анимация на кнопке видео
 3. Стилизовать fancybox
+
+
+
+Your site is live at https://kamen-raven.github.io/real-estate-Turkey/
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import { Fancybox } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import FancyBox from './libs/fancyapps/fancybox';
+import animatedScrollLib from './libs/aos/aos';
 
-Fancybox.bind("[data-fancybox]", {
-/*   tpl: {
-    closeButton:
-      '<button data-fancybox-close class="f-button is-close-btn" title="Закрыть">' +
-      '<svg width="15" height="15" viewBox="15" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="15" height="2.7" fill="white" /><rect y="6" x="3" width="12" height="2.7" fill="white" /><rect y="12" x="6" width="9" height="2.7" fill="white" /></svg>' +
-      "</button>",
-  }, */
-});
-
-
-/* function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
-
-
-setupCounter(document.querySelector('#counter'))
- */
+FancyBox();
+animatedScrollLib();
