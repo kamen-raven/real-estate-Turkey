bd63812ac61380a0bb637a8e9706f6cd339b042e
Slava Kamenev <slava-stavropol@yandex.ru>
2023-08-12 02:17:24 +0300

    final block done

--- src/index.js
+++ src/index.js
@@ -3,3 +3,28 @@ import animatedScrollLib from './libs/aos/aos';
 
 FancyBox();
 animatedScrollLib();
+
+
+
+const acc = document.querySelectorAll(".accordion");
+
+
+
+
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      console.log("close")
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      console.log("open")
+    }
+  });
+
+}
+
