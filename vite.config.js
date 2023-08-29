da819f2e33591862d8d28b389b535885f6de0c30
Slava Kamenev <slava-stavropol@yandex.ru>
2023-08-30 02:03:22 +0300

    adaptive gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import sass from 'sass';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: 'real-estate-Turkey',
   css: {
     preprocessorOptions: {
       scss: {
